3c36af50b151c8e0211b168c6762daa7ca6bc430
jedi24garcia <jedi24garcia@gmail.com>
2024-07-19 20:04:03 +1200

    modified

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import turtle
 pen = turtle.Turtle()
+pen.fillcolor("red")
 turtle.bgcolor('black')
 pen.speed(0)
-pen.colors = ["red", "yellow", "blue", "green", "purple", "orange"]
+pen.colors = ["red"]
 for i in range(300):
-    pen.pencolor(pen.colors[i % 6])
+    pen.pencolor(pen.colors[i % 1])
     pen.forward(i * 2)
     pen.right(61)
+
+
 turtle.done()
