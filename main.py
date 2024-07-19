09b8cc7085a3f188f17b0c51cbb0909f713865c9
jedi24garcia <jedi24garcia@gmail.com>
2024-07-19 18:59:47 +1200

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import turtle
 pen = turtle.Turtle()
 turtle.bgcolor('black')
-pen.speed(1)
+pen.speed(0)
 pen.colors = ["red", "yellow", "blue", "green", "purple", "orange"]
 for i in range(300):
     pen.pencolor(pen.colors[i % 6])
